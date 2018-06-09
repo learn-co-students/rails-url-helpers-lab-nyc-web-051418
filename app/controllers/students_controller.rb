119ff3ba1dc2e3ba12306c819ad59858128fb552
github username <github email address>
2018-06-09 15:29:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    toggle_status = @student.activate
+    @student.update(active: toggle_status)
+    redirect_to action: show
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    if self.active == false
+      true
+    else
+      false
+    end
+  end
+
 end
