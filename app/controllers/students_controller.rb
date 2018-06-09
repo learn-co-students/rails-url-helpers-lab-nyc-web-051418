d8d4b9fb39c50325d87d5c565fa2a239950274fa
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-09 12:35:08 -0400

    Solved lab ' exit ;

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,32 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    @student = set_student
+    @student.active = !@student.active
+    @student.save
+    redirect_to :student
+
+  end
+
+  def update
+    set_student
+    params[:student][:active] ||= false
+    @student.update(active: params[:student][:active])
+    redirect_to :student
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
